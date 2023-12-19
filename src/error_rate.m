7f581174b6371aaac13688d1f0f3fc96296b258f
Sergejs Umnovs <sergio30032001@gmail.com>
2023-12-19 02:50:31 +0200

    Fixed some issues

--- src/error_rate.m
+++ src/error_rate.m
@@ -5,7 +5,7 @@ N = 50; % samples of a single symbol
 
 %% Barker signal generation
 barker_sig = [1 1 -1];
-input_sig = [kron(barker_sig, ones(1, N)) zeros(1, N*7)];
+input_sig = [kron(barker_sig, ones(1, N))];
 P_avg_sig = rms(input_sig)^2;
 
 %% Matched filter
@@ -29,7 +29,7 @@ for std_dev = 0.1:0.1:10
         noise = std_dev*randn(1, length(input_sig));
         input_sig_noise = input_sig + noise;
         bs_filt = filter(b, 1, input_sig_noise);
-        sampled(i) = bs_filt(150) >= threshold;
+        sampled(i) = bs_filt(length(input_sig)) >= threshold;
     end
     err_rate = [err_rate mean(ones(1,100)-sampled)];
 end
